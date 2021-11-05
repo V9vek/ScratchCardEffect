5a22d176b3d93e9853edb78d4bd6b1042148f889
Vivek Sharma <viveksharma2382000@gmail.com>
2021-11-05 23:24:44 +0530

    minor changes

--- app/src/main/java/com/vivek/scratchcardeffect/models/DraggedPath.kt
+++ app/src/main/java/com/vivek/scratchcardeffect/models/DraggedPath.kt
@@ -1,10 +1,8 @@
 package com.vivek.scratchcardeffect.models
 
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 
 data class DraggedPath(
     val path: Path,
-    val color: Color = Color.White,
     val width: Float = 50f
 )
